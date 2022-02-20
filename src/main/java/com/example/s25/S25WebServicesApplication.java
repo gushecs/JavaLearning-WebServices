012c9be12cd527587be87f49a9f692f534816283
Gustavo Santos <gustavo.heitor@outlook.com>
2022-02-20 13:06:11 -0300

    Project creation

---
+++ src/main/java/com/example/s25/S25WebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.s25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S25WebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S25WebServicesApplication.class, args);
+	}
+
+}
